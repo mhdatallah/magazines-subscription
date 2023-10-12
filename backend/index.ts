75836a02b8fa2d812294df7d9609386d54801a29
Mohammad Atallah <atal.mohd@gmail.com>
2023-10-12 12:54:28 +0400

    Implement GET /magazines & /magazines/:id

--- backend/index.ts
+++ backend/index.ts
@@ -1,14 +1,32 @@
-import express, { Request, Response , Application } from 'express';
-import dotenv from 'dotenv';
+import express, {
+  Request,
+  Response,
+  ErrorRequestHandler,
+} from "express";
+import dotenv from "dotenv";
+import { getMagazine, getMagazines } from "./database";
 
-//For env File 
+//For env File
 dotenv.config();
 
-const app: Application = express();
+const app = express();
 const port = process.env.PORT || 8000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Welcome to Express & TypeScript Server');
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  console.error(err);
+  res.status(500).send("Oops 😵");
+};
+app.use(errorHandler);
+
+app.get("/magazines", async (req: Request, res: Response) => {
+  const magazines = await getMagazines();
+  res.send(magazines);
+});
+
+app.get("/magazines/:id", async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const magazine = await getMagazine(+id);
+  res.send(magazine);
 });
 
 app.listen(port, () => {

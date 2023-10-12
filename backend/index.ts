1f7ff8ec9287260b7a3f24f77b7b76529f001eae
Mohammad Atallah <atal.mohd@gmail.com>
2023-10-12 16:22:17 +0400

    Implement POST /magazines & /magazines/:id

--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,6 @@
-import express, {
-  Request,
-  Response,
-  ErrorRequestHandler,
-} from "express";
+import express, { Request, Response, ErrorRequestHandler } from "express";
 import dotenv from "dotenv";
-import { getMagazine, getMagazines } from "./database";
+import { createMagazine, getMagazine, getMagazines, updateMagazine } from "./database";
 
 //For env File
 dotenv.config();
@@ -12,11 +8,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
 
-const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
-  console.error(err);
-  res.status(500).send("Oops 😵");
-};
-app.use(errorHandler);
+app.use(express.json());
 
 app.get("/magazines", async (req: Request, res: Response) => {
   const magazines = await getMagazines();
@@ -26,6 +18,18 @@ app.get("/magazines", async (req: Request, res: Response) => {
 app.get("/magazines/:id", async (req: Request, res: Response) => {
   const id = req.params.id;
   const magazine = await getMagazine(+id);
+  res.send(magazine);
+});
+
+app.post("/magazines", async (req: Request, res: Response) => {
+  const { title } = req.body;
+  const magazine = await createMagazine({ title });
+  res.send(magazine);
+});
+
+app.post("/magazines/:id", async (req: Request, res: Response) => {
+  const { title, id, is_deleted, is_subscribed } = req.body;
+  const magazine = await updateMagazine({ id, title, is_deleted, is_subscribed });
   res.send(magazine);
 });
 

34ee6606e3c18f663a431f62939dca1831454fc9
Mohammad Atallah <atal.mohd@gmail.com>
2023-10-12 16:29:27 +0400

    Implement DELETE /magazines/:id

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,6 @@
-import express, { Request, Response, ErrorRequestHandler } from "express";
+import express, { Request, Response } from "express";
 import dotenv from "dotenv";
-import { createMagazine, getMagazine, getMagazines, updateMagazine } from "./database";
+import { createMagazine, deleteMagazine, getMagazine, getMagazines, updateMagazine } from "./database";
 
 //For env File
 dotenv.config();
@@ -10,7 +10,7 @@ const port = process.env.PORT || 8000;
 
 app.use(express.json());
 
-app.get("/magazines", async (req: Request, res: Response) => {
+app.get("/magazines", async (_req: Request, res: Response) => {
   const magazines = await getMagazines();
   res.send(magazines);
 });
@@ -24,13 +24,17 @@ app.get("/magazines/:id", async (req: Request, res: Response) => {
 app.post("/magazines", async (req: Request, res: Response) => {
   const { title } = req.body;
   const magazine = await createMagazine({ title });
-  res.send(magazine);
+  res.status(201).send(magazine);
 });
 
 app.post("/magazines/:id", async (req: Request, res: Response) => {
   const { title, id, is_deleted, is_subscribed } = req.body;
   const magazine = await updateMagazine({ id, title, is_deleted, is_subscribed });
   res.send(magazine);
+});
+
+app.delete("/magazines/:id", async (_req: Request, res: Response) => {
+  res.sendStatus(204);
 });
 
 app.listen(port, () => {

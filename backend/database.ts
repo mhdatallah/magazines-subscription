34e92caf2b31cc0a4657c881b731015396e35935
Mohammad Atallah <atal.mohd@gmail.com>
2023-10-12 11:00:25 +0400

    Implement getMagazine

--- backend/database.ts
+++ backend/database.ts
@@ -1,5 +1,5 @@
 import { createPool } from "mysql2";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -7,10 +7,22 @@ const pool = createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database:process.env.MYSQL_DATABASE,
+  database: process.env.MYSQL_DATABASE,
 }).promise();
 
 export const getMagazines = async () => {
-  const [rows] = await pool.query("SELECT * FROM magazines");
+  const [rows] = await pool.query(`SELECT * FROM magazines`);
   return rows;
+};
+
+export const getMagazine = async (id: number) => {
+  const [row] = await pool.query(
+    `
+  SELECT *
+  FROM magazines
+  WHERE id = ?
+  `,
+    [id]
+  );
+  return row;
 };

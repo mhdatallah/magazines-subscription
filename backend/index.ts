b755d01c2152dbc5c70fbd1d34cd5db026918b97
Mohammad Atallah <atal.mohd@gmail.com>
2023-10-13 10:48:31 +0400

    Handle CORS

--- backend/index.ts
+++ backend/index.ts
@@ -14,6 +14,13 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(express.json());
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 
 app.get("/api/v1/magazines", async (_req: Request, res: Response) => {
   const magazines = await getMagazines();

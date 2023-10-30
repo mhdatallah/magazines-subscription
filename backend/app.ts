b5b1ce3bc27f201eea7ac40ab52ddb77b5534ad1
Mohammad Atallah <atal.mohd@gmail.com>
2023-10-30 20:53:20 +0400

    Install cors

--- backend/app.ts
+++ backend/app.ts
@@ -1,13 +1,12 @@
 import express from "express";
-import { configureCORS } from "./middleware/cors";
+import cors from 'cors';
 import magazineRoutes from "./routes/magazines";
 import userRoutes from "./routes/users";
 import subscriptionRoutes from "./routes/subscriptions";
 
 const app = express();
 app.use(express.json());
-
-app.use(configureCORS);
+app.use(cors());
 
 app.use("/api/v1/magazines", magazineRoutes);
 app.use("/api/v1/users", userRoutes);
--- backend/middleware/cors.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-
-export const configureCORS = (req: Request, res: Response, next: NextFunction) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  next();
-};
